800c7666d746fbc0d4661a4e477f62a75566b79c
Barış SAVAŞ <barissavas17@gmail.com>
2022-12-04 19:06:13 +0300

    edited

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -19,17 +19,43 @@ export class LoginComponent implements OnInit {
 
   stateOptions: any[] = [];
   value1: string = "off";
-  
+  uploadedFiles: any[] = [];
+  fileToUpload : any;
+imageUrl:string =''
+
+
+
   ngOnInit(): void {
-    this.stateOptions = [{label: 'Off', value: 'off'}, {label: 'On', value: 'on'}];
+    this.stateOptions = [{label: 'SIGN UP', value: 'off'}, {label: 'LOGIN', value: 'on'}];
   }
 
   addUser() {
     console.log(this.userForm.value)
   }
 
+  handleFileInput(file: FileList) {
+    this.fileToUpload = file.item(0);
+
+    //Show image preview
+    let reader = new FileReader();
+    reader.onload = (event: any) => {
+      this.imageUrl = event.target.result;
+    }
+    reader.readAsDataURL(this.fileToUpload);
+  }
 
 
+  onUpload(event : any) {
+    for (let file of event.files) {
+        this.uploadedFiles.push(file);
+    }
+
+}
+
+onBasicUploadAuto(event : any) {
+  console.log(event)
+}
+}
 
 
 
@@ -60,4 +86,3 @@ export class LoginComponent implements OnInit {
       zip: new FormControl('')
     })
   }); */
-}
